3470d6aff3119d597fcc6efc6eed45b0a1e51c46
Dhanu Satrio <satriodhanu12@gmail.com>
2023-09-10 20:44:44 +0700

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -12,7 +12,7 @@ const Project = () => {
     <div id='project' className='flex flex-col md:flex-row-reverse md:gap-12 lg:gap-16 px-4 bg-white dark:bg-neutral-900'>
         <div className='max-w-[1040px] m-auto md:pl-20 p-4 py-7 mb-16'>
             <h1 className='text-4xl font-bold text-center  text-neutral-800 dark:text-neutral-100'>Projects</h1>
-            <p className='text-center py-8 text-neutral-600 dark:text-neutral-400'>
+            <p className='py-8 text-neutral-600 dark:text-neutral-400 text-justify'>
             In my journey as a programmer, I've created projects and tackled bugs, all with the aim of making a dent in the digital universe. These experiences have been instrumental in my growth as a developer, allowing me to continually push boundaries and expand my skill set.
             </p>
             <div className='grid sm:grid-cols-2 gap-12'>
